bcdcdcd77f9e532ea876683ff117e9d551c09a8a
Rafael Vieira Coelho <rafaelvc2@gmail.com>
2020-04-12 11:13:50 -0300

    Add files via upload

---
+++ Aula04_Kotlin/src/Ex2FuncoesInfix.kt
@@ -0,0 +1,30 @@
+fun main() {
+
+    infix fun Int.times(str: String) = str.repeat(this)        // 1
+    println(2 times "Bye ")                                    // 2
+
+    val pair = "Ferrari" to "Katrina"                          // 3
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)   // 4
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+    val claudia = Person("Claudia")
+    sophia likes claudia                                       // 5
+    println(sophia.name + " likes " + sophia.toString())
+    println(claudia.name + " likes " + claudia.toString())
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) { likedPeople.add(other) }  // 6
+    override fun toString(): String {
+        var t = ""
+        for (p in likedPeople) {
+            t += p.name
+        }
+        return t
+    }
+}
