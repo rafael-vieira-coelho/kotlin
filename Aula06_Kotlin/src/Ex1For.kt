cefe6e2934ce94f1fd8e7dc4c54e12777b4923a4
Rafael Vieira Coelho <rafaelvc2@gmail.com>
2020-04-13 11:57:51 -0300

    Add files via upload

---
+++ Aula06_Kotlin/src/Ex1For.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {                               // 1
+        println("Yummy, it's a $cake cake!")
+    }
+}
+
